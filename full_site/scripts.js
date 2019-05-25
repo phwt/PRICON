84d8e7a9fd08c6b922a4b6bdb8f81d779ffe2a4e
phwt <phwt.smwt@gmail.com>
2019-05-25 22:06:42 +0700

    Ads functionality

--- full_site/scripts.js
+++ full_site/scripts.js
@@ -1,5 +1,22 @@
+function scrollToAnchor(aid){
+    var aTag = $("[sname='"+ aid +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+}
+
+function selNone(){
+    showAds(none);
+}
 $(document).ready(function() {
+    var selected="";
     $(".prod-sel").click(function(){
-        alert($(this).find("span.typ").text());
+        selected = $(this).find("span.typ").text();
+        showAds(selected);
     });
+
+    function showAds(type){
+        scrollToAnchor('ads');
+        $("#ads-1").attr("src", "ads/ads_"+type+"1.jpg");
+        $("#ads-2").attr("src", "ads/ads_"+type+"2.jpg");
+    }
+
 });
